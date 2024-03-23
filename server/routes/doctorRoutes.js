3d8d4ee7bc760a24ae41edf50fda794fa41a5229
samhithapentaparthy <122796043+samhithapentaparthy@users.noreply.github.com>
2024-03-22 22:43:02 -0400

    fixed typos

--- server/routes/doctorRoutes.js
+++ server/routes/doctorRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express")
 
-
 const { signInController } = require("../controllers/doctorController")
 
 const router = express.Router()
 
 router.post("/signin", signInController)
 
-models.export = router;
+module.exports = router;
